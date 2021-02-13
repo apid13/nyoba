76bc9af88220a4ebaed8a542802c3f3f4d3e6792
apid13 <78915816+apid13@users.noreply.github.com>
2021-02-13 18:08:26 +0700

    Update imagemaker.js

--- database/menu/imagemaker.js
+++ database/menu/imagemaker.js
@@ -8,23 +8,6 @@ const imagemaker = (prefix, botName, ownerName) => {
    ┠≽ *Creator* : ${ownerName}
    ┠≽ *Version* : 0.0.5
    ╿
-┯┷ *〈 ABOUT 〉*
-╽
-┠≽ *${prefix}info* (error)
-┃ *Desc* : Show Bot Details
-┠──────────────╼
-┠≽ *${prefix}blocklist*
-┃ *Desc* : Show Blocked User
-┠──────────────╼
-┠≽ *${prefix}chatlist* (error)
-┃ *Desc* : Show All Chat User
-┠──────────────╼
-┠≽ *${prefix}ping*
-┃ *Desc* : Show Connection Bot Speed
-┠──────────────╼
-┠≽ *${prefix}bugreport* <text>
-┃ *Desc* : Report Bug To Owner Bot
-╿
 ┷┯ *〈 IMAGE MAKER 〉*
    ╽
    ┠≽ *${prefix}bpink* <text>
